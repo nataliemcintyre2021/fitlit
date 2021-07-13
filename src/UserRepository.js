126bbf2d850acdde60ca35f61a10a70cf650f83a
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-07-13 07:39:56 -0700

    Update user step method

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -2,6 +2,16 @@ class UserRepository {
   constructor(theData) {
     this.data = theData
   }
+
+  getDataById() {
+
+  }
+
+  getAllUserAverageStepGoal() {
+    this.data.reduce((acc, user) => {
+      return acc + user["dailyStepGoal"]
+    }, 0)/this.data.length;
+  }
 }
 
 export default UserRepository;
---
+++ test/User-test.js
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import User from '../src/User';
+
+describe('User', () => {
+  it('should be a function', function () {
+    expect(User).to.be.a('function');
+  });
+
+  it('should have a parameter to take in a userData object', function () {
+    const user1 = new User(
+      {
+        "id": 1,
+        "name": "Luisa Hane",
+        "address": "15195 Nakia Tunnel, Erdmanport VA 19901-1697",
+        "email": "Diana.Hayes1@hotmail.com",
+        "strideLength": 4.3,
+        "dailyStepGoal": 10000,
+        "friends": [
+          16,
+          4,
+          8
+        ]
+      });
+      expect(user1).to.be.an.instanceof(User);
+      console.log(user1);
+  })
+
+})
