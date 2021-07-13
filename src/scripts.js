8ab95ed50e8e50c7fc5f7ab7c6bdc998e956752d
Megan McBride <megan.mcbride@colorado.edu>
2021-07-13 16:53:03 -0600

    Display user's first name in welcome and add functionality to display the User Information Page

--- src/scripts.js
+++ src/scripts.js
@@ -19,3 +19,64 @@ console.log('This is the JavaScript entry file - your code begins here.');
 
 import userData from './data/users';
 import UserRepository from './UserRepository';
+import User from './User';
+
+
+//query selectors
+const userWelcome = document.getElementById('user-welcome');
+const stepComparison = document.getElementById('step-comparison');
+const flameLogo = document.getElementById('flame-logo');
+const userInfoBtn = document.getElementById('user-info-btn');
+const landingPage = document.getElementById('main-dashboard');
+const userInfoPage = document.getElementById('user-info-page');
+
+
+//global variable
+let currentUser;
+
+
+//event listeners
+window.addEventListener('load', displayUserWelcome);
+userInfoBtn.addEventListener('click', displayUserInformationPage);
+flameLogo.addEventListener('click', displayMainLandingPage)
+
+//functions
+function displayUserWelcome() {
+  let newUser = {
+    "id": 43,
+    "name": "Alfonso Sporer",
+    "address": "584 Mayert Greens, West Arden SC 97033",
+    "email": "Jadon_Borer@gmail.com",
+    "strideLength": 4,
+    "dailyStepGoal": 5000,
+    "friends": [
+      2,
+      19,
+      43,
+      33
+    ]
+  }
+
+  currentUser = new User(newUser);
+  userWelcome.innerText = `Welcome, ${currentUser.returnUserFirstName()} !`;
+  displayStepsComparison();
+};
+
+function displayStepsComparison() {
+  stepComparison.innerText = `Your step goal is ${currentUser.dailyStepGoal}, compared to your
+  fellow users' average goal of 6667`;
+}
+
+function displayUserInformationPage() {
+  toggleView(userInfoPage, landingPage);
+
+}
+
+function displayMainLandingPage() {
+  
+}
+
+function toggleView(show, hide) {
+  show.classList.remove('hidden');
+  hide.classList.add('hidden');
+}
