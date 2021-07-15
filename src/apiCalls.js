e12a83c45a9484e9b7895c7e2e9cd4279b13ef91
Natalie McIntyre <nataliemcintyre2021@gmail.com>
2021-07-14 19:23:13 -0700

    Add api fetch calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,8 +1,23 @@
-const getData = () => {
+export const getUserData = () => {
   return fetch('http://localhost:3001/api/v1/users')
   .then(response => response.json())
   .then(data => data)
 }
 
+export const getSleepData = () => {
+  return fetch('http://localhost:3001/api/v1/sleep')
+  .then(response => response.json())
+  .then(data => data)
+}
 
-export default {getData}
+export const getActivityData = () => {
+  return fetch('http://localhost:3001/api/v1/activity')
+  .then(response => response.json())
+  .then(data => data)
+}
+
+export const getHydrationData = () => {
+  return fetch('http://localhost:3001/api/v1/hydration')
+  .then(response => response.json())
+  .then(data => data)
+}
